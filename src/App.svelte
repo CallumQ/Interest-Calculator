9274cd2e3b430b6d69fbe036dd8413ca18f113f3
Callum Quigley <super-callum@hotmail.com>
2022-01-04 00:38:51 +0000

    Added support for annual deposits

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script>
-	let initial_sum = 10000;
-	let interest_rate = 7;
+	let initial_sum = 0;
+	let interest_rate = 1;
 	let number_of_years = 1;
+	let annual_deposit = 0;
 	let currency_symbol = "$";
 	let total_sum = 0;
 
@@ -9,7 +10,7 @@
 		let running_total = initial_sum;
 
 		for(let i = 0; i < number_of_years; i++) {
-			running_total = running_total * (interest_rate / 100 + 1);
+			running_total = running_total + annual_deposit * (interest_rate / 100 + 1);
 		}
 
 		total_sum = Math.round((running_total + Number.EPSILON) * 100) / 100
@@ -24,45 +25,74 @@
 <main>
 	<div class="container">
 		<div class="row">
-		  <div class="col-8 offset-md-2">
+		  <div class="col-6 offset-md-3">
 			<h1>Interest Calculator</h1>
 			
 			<form>
 				<div class="mb-2 row">
 					<label for="currency" class="col-sm-4 col-form-label">Currency</label>
-					<div class="col-sm-8">
-						<div class="btn-group" role="group" aria-label="Basic example" on:click={setActiveCurrency}>
-							<button type="button" class="btn btn-primary">$</button>
-							<button type="button" class="btn btn-outline-primary">£</button>
-							<button type="button" class="btn btn-outline-primary">€</button>
-							<button type="button" class="btn btn-outline-primary">¥</button>
-							<button type="button" class="btn btn-outline-primary">₹</button>	
+					<div class="col-sm-6">
+						<div class="btn-group" role="group" aria-label="Basic example">
+							<button type="button" class="btn btn-primary" on:click={setActiveCurrency}>$</button>
+							<button type="button" class="btn btn-outline-primary" on:click={setActiveCurrency}>£</button>
+							<button type="button" class="btn btn-outline-primary" on:click={setActiveCurrency}>€</button>
+							<button type="button" class="btn btn-outline-primary" on:click={setActiveCurrency}>¥</button>
+							<button type="button" class="btn btn-outline-primary" on:click={setActiveCurrency}>₹</button>
+
+							<!-- <button id="otherCurrencyDropdown" type="button" class="btn btn-outline-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Other</button>
+							<ul class="dropdown-menu" aria-labelledby="otherCurrencyDropdown">
+								<li>
+									<div class="row">
+										<label for="otherCurrencySymbol" class="col-sm-6 col-form-label">Custom symbol</label>
+										<div class="col-sm-6">
+											<div class="input-group">
+												<input type="string" id="otherCurrencySymbol" name="otherCurrencySymbol" bind:value={currency_symbol} class="form-control">
+											</div>
+										</div>
+									</div>
+								</li>
+							</ul>	 -->
 						</div>
 					</div>
 				</div>
 				<div class="mb-2 row">
 					<label for="initialInvestment" class="col-sm-4 col-form-label">Initial investment</label>
-					<div class="col-sm-8">
-						<input type="number" name="initialInvestment" bind:value={initial_sum}>
+					<div class="col-sm-6">
+						<div class="input-group">
+							<div class="input-group-prepend">
+								<span class="input-group-text">{currency_symbol}</span>
+							  </div>
+							<input type="number" id="initialInvestment" name="initialInvestment" bind:value={initial_sum} class="form-control">
+						</div>
 					</div>
 				</div>
 				<div class="mb-2 row">
 					<label for="interestRate" class="col-sm-4 col-form-label">Interest rate</label>
-					<div class="col-sm-8">
-						<input type="number" name="interestRate" bind:value={interest_rate}>
+					<div class="col-sm-6">
+						<div class="input-group">
+							<input type="number" id="interestRate" name="interestRate" bind:value={interest_rate} class="form-control">
+							<span class="input-group-text">%</span>
+						</div>
+
 					</div>
 				</div>
 				<div class="mb-2 row">
 					<label for="noOfYears" class="col-sm-4 col-form-label">Number of years</label>
-					<div class="col-sm-8">
-						<input type="number" name="noOfYears" bind:value={number_of_years}>
+					<div class="col-sm-6">
+						<input type="number" id="noOfYears" name="noOfYears" bind:value={number_of_years} class="form-control">
+					</div>
+				</div>
+				<div class="mb-2 row">
+					<label for="annualDeposit" class="col-sm-4 col-form-label">Annual deposit</label>
+					<div class="col-sm-6">
+						<input type="number" id="annualDeposit" name="annualDeposit" bind:value={annual_deposit} class="form-control">
 					</div>
 				</div>
 				<input type="button" on:click={CalculateTotal} value="Calculate" class="btn btn-primary">
 			</form>
 
 			{#if total_sum}
-				<h2> Result:</h2>
+				<h2> Result</h2>
 				<p>Total value after {number_of_years} years: {currency_symbol + total_sum}</p>
 			{/if}
 		  </div>
